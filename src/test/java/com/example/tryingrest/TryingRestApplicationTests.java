a1eacaf42db71be2bd8274cd0192792a49ee7ab3
dejerzCode <dejerzcode@gmail.com>
2022-05-29 20:09:54 -0600

    Initial files

---
+++ src/test/java/com/example/tryingrest/TryingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tryingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryingRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
